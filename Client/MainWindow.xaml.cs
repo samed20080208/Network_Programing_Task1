66f095d2dab4e7f3ad15d9824d3635cbe155b4fb
lenovo <lenovo@DESKTOP-O95QVK8>
2024-09-28 22:12:22 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.IO;
+
+namespace Client;
+
+public partial class MainWindow : Window
+{
+    private readonly UdpClient _udpClient;
+    private readonly IPEndPoint _serverEndpoint;
+    private DispatcherTimer _timer;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _udpClient = new UdpClient();
+        _serverEndpoint = new IPEndPoint(IPAddress.Loopback, 27001);
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            btnStart.IsEnabled = false;
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(2)
+            };
+            _timer.Tick += async (s, args) => await UpdateScreenshot();
+            _timer.Start();
+
+            await UpdateScreenshot();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+        finally
+        {
+            btnStart.IsEnabled = true;
+        }
+    }
+
+    private async Task UpdateScreenshot()
+    {
+        var receivedBuffer = new byte[ushort.MaxValue - 29];
+        await _udpClient.SendAsync(receivedBuffer, receivedBuffer.Length, _serverEndpoint);
+
+        var list = new List<byte>();
+        try
+        {
+            while (true)
+            {
+                var result = await _udpClient.ReceiveAsync();
+                var receivedData = result.Buffer;
+                list.AddRange(receivedData);
+
+                if (receivedData.Length < receivedBuffer.Length) break;
+            }
+
+            try
+            {
+                var image = ByteArrayToImage([.. list]);
+                ImageBox.Source = image;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error converting byte array to image: {ex.Message}");
+                _timer.Stop();
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error receiving data: {ex.Message}");
+            _timer.Stop();
+        }
+    }
+
+    private BitmapImage ByteArrayToImage(byte[] byteArray)
+    {
+        if (byteArray == null || byteArray.Length == 0) return null;
+
+        var image = new BitmapImage();
+        using (var m = new MemoryStream(byteArray))
+        {
+            m.Position = 0;
+            image.BeginInit();
+            image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = m;
+            image.EndInit();
+        }
+        image.Freeze();
+        return image;
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+class Program
+{
+    static async Task Main()
+    {
+        using UdpClient udpServer = new(27001);
+        IPEndPoint localEndpoint = new(IPAddress.Parse("192.168.1.8"), 27001);
+        Console.WriteLine($"Server started on {localEndpoint}");
+
+        while (true)
+        {
+            UdpReceiveResult result = await udpServer.ReceiveAsync();
+
+            while (true)
+            {
+                Bitmap screenshot = GetScreenshot();
+                byte[] imageData = ImageToByteArray(screenshot);
+
+                if (imageData.Length / 1024f >= 1000)
+                    Console.WriteLine($"{(imageData.Length / 1024f) / 1024f} mb");
+                else
+                    Console.WriteLine($"{imageData.Length / 1024f} kb");
+
+                var chunk = imageData.Chunk(ushort.MaxValue - 29);
+                var buffer = chunk.ToArray();
+
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    await Task.Delay(30);
+                    await udpServer.SendAsync(buffer[i], buffer[i].Length, result.RemoteEndPoint);
+                }
+
+                Console.WriteLine($"Sent screenshot to {result.RemoteEndPoint}");
+                Console.WriteLine();
+
+                await Task.Delay(2000);
+            }
+
+        }
+    }
+
+    static Bitmap GetScreenshot()
+    {
+        Bitmap? screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,Screen.PrimaryScreen.Bounds.Height)!;
+
+        Graphics graphics = Graphics.FromImage(screenshot);
+        graphics.CopyFromScreen(0, 0, 0, 0, screenshot.Size);
+
+        return screenshot;
+    }
+
+    static byte[] ImageToByteArray(Image image)
+    {
+        using (MemoryStream stream = new())
+        {
+            image.Save(stream, ImageFormat.Png);
+            return stream.ToArray();
+        }
+    }
+}
+
